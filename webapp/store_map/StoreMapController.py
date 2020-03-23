7ea5d19459df9fcdaa44d6ddac3c9795c124b4a6
Ernesto Ruge <mail@ernestoruge.de>
2020-03-23 14:44:26 +0000

    remove login for map

--- webapp/store_map/StoreMapController.py
+++ webapp/store_map/StoreMapController.py
@@ -19,6 +19,5 @@ store_map = Blueprint('store_map', __name__, template_folder='templates')
 
 @store_map.route('/store-map')
 @store_map.route('/')
-@login_required
 def regions_main():
     return render_template('map.html')
